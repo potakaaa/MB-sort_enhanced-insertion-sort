6f1e62139359d998f504f79146372781de3fdb35
unknown <helbirog@gmail.com>
2024-10-26 10:03:52 +0800

    Added testcase function

--- 2pointer_selection.py
+++ 2pointer_selection.py
@@ -1,56 +1,50 @@
+import random
+from algo_testcases import test_algo_works
+
 def enhanced_selection(arr):
-    
-    p1 = 1
-    p2 = len(arr) - 1
-
-    temp_first = 1
-    temp_last = len(arr) - 1
     # for even number of elements only
-    for i in range(len(arr) // 2):
-        min_idx = i
-        max_idx = i
-        minChange = False
-        maxChange = False
-        print(arr)
+    
+    for i in range((len(arr) // 2)):
+        last = len(arr) - 1 - i
+        min_idx = i 
+        max_idx = len(arr) - 1 - i
+        
+        #print(arr)
 
-        for j in range(1, len(arr) // 2):
-            print(p1)
-            print(p2)
+        for j in range(i+1, (len(arr) // 2) + 1):
+            p1 = j
+            p2 = len(arr) - j
+            #print("i = ", i, "j = ", j)
             if arr[p1] < arr[min_idx] or arr[p2] < arr[min_idx]:
                 if arr[p1] < arr[min_idx]:
                     min_idx = p1
-                    print("Min Index: ", min_idx)
+                    print("p1 at ", p1," found Min Index: ", min_idx, " with value ", arr[min_idx])
                 if arr[p2] < arr[min_idx]:
                     min_idx = p2
-                    print("Min Index: ", min_idx)
-                minChange = True
+                    #print("p2 at ", p2," found Min Index: ", min_idx, " with value ", arr[min_idx])
+
             if arr[p1] > arr[max_idx] or arr[p2] > arr[max_idx]:
                 if arr[p1] > arr[max_idx]:
                     max_idx = p1
-                    print("Max Index: ", max_idx)
+                    #print("p1 at ", p1, " found Max Index: ", max_idx, " with value ", arr[max_idx])
+
                 if arr[p2] > arr[max_idx]:
                     max_idx = p2
-                    print("Max Index: ", max_idx)
-                maxChange = True
+                    #print("p2 at ", p2," found Max Index: ", max_idx, " with value ", arr[max_idx])
+            #print("p1: ", p1, ", p2: ", p2)
 
-            p1 += 1
-            p2 -= 1
-        
-
-        if minChange:
+            #print("Minimum Index: ", min_idx, ", Maximum Index: ", max_idx)
+        if min_idx != i:
+            #print("swapping index ", min_idx, " = ", arr[min_idx], " with index ", i, " = ", arr[i])
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        if maxChange:
-            arr[temp_last], arr[max_idx] = arr[max_idx], arr[temp_last]
-        print(arr)
-        
-        temp_first += 1
-        temp_last -= 1
+            if max_idx == i:
+                max_idx = min_idx
 
-        minChange = False
-        maxChange = False
+        if max_idx != last:
+            #print("swapping index ", max_idx, " = ", arr[max_idx], " with index ", last, " = ", arr[last])
+            arr[last], arr[max_idx] = arr[max_idx], arr[last]
+        #print(arr)
 
-        p1 = 1
-        p2 = len(arr) - 1
     
     return arr
 
@@ -59,9 +53,28 @@ def enhanced_selection(arr):
 
         
 
-arr1 = [2, 3, 1, 2, 4, 3]
+arrn = [10, 2, 18, 36, 52, 68, 84, 100, 14, 28, 
+        42, 58, 74, 90, 6, 20, 34, 48, 62, 76, 
+        92, 8, 22, 36, 50, 64, 78, 94, 12, 26, 
+        40, 54, 68, 82, 96, 16, 30, 44, 58, 72, 
+        86, 100, 18, 32, 46, 60, 74]
 
-print(enhanced_selection(arr1))
 
+n = 10
+
+arr1 = random.sample(range(1, 101), n)
+arr1_copy = arr1.copy()
+arr2 = random.sample(range(1, 101), n)
+arr2_copy = arr2.copy()
+arr3 = random.sample(range(1, 101), n)
+arr3_copy = arr3.copy()
+arr4 = random.sample(range(1, 101), n)
+arr4_copy = arr4.copy()
+arr5 = random.sample(range(1, 101), n)
+arr5_copy = arr5.copy()
+
+print("\n\n\n")
+
+test_algo_works(enhanced_selection, "Selection", arr1)
 
             
--- algo_testcases.py
+++ algo_testcases.py
@@ -13,4 +13,18 @@ def test_algo(algo, algo_name, arr):
 
 
     print("Algo is Correct" if sorted(arr) == arr_sorted[0] else "Algo is Not Correct")
+
+
+def test_algo_works(algo, algo_name, arr):
+    print("Unsorted: ", arr)
+
+    enhanced = algo(arr)
+
+    print("Normal selection sorted: ", sorted(arr))
+    print("Enhanced selection sorted: ", enhanced)
+
+    print(f"Enhanced {algo_name} Sort is working" if enhanced == sorted(arr) else f"Enhanced {algo_name} Sort is not working")
+
+
+      
     
