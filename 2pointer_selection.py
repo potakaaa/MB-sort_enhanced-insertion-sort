b1673c8d07f18f55eafc507c8255cf772b26e4ea
unknown <delmundo.hansmatthew@gmail.com>
2024-10-26 09:37:57 +0800

    cleaned some stuff

--- 2pointer_selection.py
+++ 2pointer_selection.py
@@ -31,7 +31,6 @@ def enhanced_selection(arr):
             print("p1: ", p1, ", p2: ", p2)
 
             print("Minimum Index: ", min_idx, ", Maximum Index: ", max_idx)
-        print(arr)
         if min_idx != i:
             print("swapping index ", min_idx, " = ", arr[min_idx], " with index ", i, " = ", arr[i])
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
@@ -51,7 +50,11 @@ def enhanced_selection(arr):
 
         
 
-arr1 = [10, 2, 18, 36, 52, 68, 84, 100, 14, 28, 42, 58, 74, 90, 6, 20, 34, 48, 62, 76, 92, 8, 22, 36, 50, 64, 78, 94, 12, 26, 40, 54, 68, 82, 96, 16, 30, 44, 58, 72, 86, 100, 18, 32, 46, 60, 74]
+arr1 = [10, 2, 18, 36, 52, 68, 84, 100, 14, 28, 
+        42, 58, 74, 90, 6, 20, 34, 48, 62, 76, 
+        92, 8, 22, 36, 50, 64, 78, 94, 12, 26, 
+        40, 54, 68, 82, 96, 16, 30, 44, 58, 72, 
+        86, 100, 18, 32, 46, 60, 74]
 
 print(enhanced_selection(arr1))
 
