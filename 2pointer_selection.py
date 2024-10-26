de3ee06d2f0cff1794d0530f927dfa2404c2b782
unknown <delmundo.hansmatthew@gmail.com>
2024-10-26 09:34:08 +0800

    fully working even functionality :,), odd needs implementation

--- 2pointer_selection.py
+++ 2pointer_selection.py
@@ -1,23 +1,24 @@
 def enhanced_selection(arr):
     # for even number of elements only
+    
     for i in range((len(arr) // 2)):
-        print("i = ", i)
         last = len(arr) - 1 - i
-        min_idx = i
+        min_idx = i 
         max_idx = len(arr) - 1 - i
-        p1 = 1 + i
-        p2 = last - 1
+        
         print(arr)
 
-        for j in range(i+1, len(arr) // 2):
-            print("j = ", j)
+        for j in range(i+1, (len(arr) // 2) + 1):
+            p1 = j
+            p2 = len(arr) - j
+            print("i = ", i, "j = ", j)
             if arr[p1] < arr[min_idx] or arr[p2] < arr[min_idx]:
                 if arr[p1] < arr[min_idx]:
                     min_idx = p1
-                    print("p1 at ", p1," p1 found Min Index: ", min_idx, " with value ", arr[min_idx])
+                    print("p1 at ", p1," found Min Index: ", min_idx, " with value ", arr[min_idx])
                 if arr[p2] < arr[min_idx]:
                     min_idx = p2
-                    print("p2 at ", p2," p2 found Min Index: ", min_idx, " with value ", arr[min_idx])
+                    print("p2 at ", p2," found Min Index: ", min_idx, " with value ", arr[min_idx])
 
             if arr[p1] > arr[max_idx] or arr[p2] > arr[max_idx]:
                 if arr[p1] > arr[max_idx]:
@@ -26,18 +27,22 @@ def enhanced_selection(arr):
 
                 if arr[p2] > arr[max_idx]:
                     max_idx = p2
-                    print("p2 at ", p2," p2 found Max Index: ", max_idx, " with value ", arr[max_idx])
+                    print("p2 at ", p2," found Max Index: ", max_idx, " with value ", arr[max_idx])
             print("p1: ", p1, ", p2: ", p2)
-            p1 += 1
-            p2 -= 1
-        
+
+            print("Minimum Index: ", min_idx, ", Maximum Index: ", max_idx)
+        print(arr)
         if min_idx != i:
+            print("swapping index ", min_idx, " = ", arr[min_idx], " with index ", i, " = ", arr[i])
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
             if max_idx == i:
                 max_idx = min_idx
+
         if max_idx != last:
+            print("swapping index ", max_idx, " = ", arr[max_idx], " with index ", last, " = ", arr[last])
             arr[last], arr[max_idx] = arr[max_idx], arr[last]
         print(arr)
+
     
     return arr
 
@@ -46,7 +51,7 @@ def enhanced_selection(arr):
 
         
 
-arr1 = [2, 3, 1, 2, 4, 3, 0, 0,5, 6, 7, 12]
+arr1 = [10, 2, 18, 36, 52, 68, 84, 100, 14, 28, 42, 58, 74, 90, 6, 20, 34, 48, 62, 76, 92, 8, 22, 36, 50, 64, 78, 94, 12, 26, 40, 54, 68, 82, 96, 16, 30, 44, 58, 72, 86, 100, 18, 32, 46, 60, 74]
 
 print(enhanced_selection(arr1))
 
