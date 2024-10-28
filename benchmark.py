423faca287d0b6c96a49a8d865cd434f4013a903
unknown <helbirog@gmail.com>
2024-10-28 10:16:53 +0800

    Added runtime difference

--- benchmark.py
+++ benchmark.py
@@ -14,11 +14,12 @@ t1 = timeit(
 print()
 print("Normal Selection runtime: ", t1)
 
-t1 = timeit(
+t2 = timeit(
     "enhanced_selection(arr1)",
     setup="from pointer_selection import enhanced_selection",
     number = 10,
     globals={"arr1":arr1},
 )
-print("Enhanced Selection runtime: ", t1)
+print("Enhanced Selection runtime: ", t2)
+print(f"Difference in runtime: {t2 - t1}" if t2 > t1 else f"Difference in runtime: {t1 - t2}")
 print()
