5be0be21f6b1c035d542803e6e1c1a36310c28e3
unknown <helbirog@gmail.com>
2024-10-28 13:19:12 +0800

    Added other sorting for comparison

--- benchmark2.py
+++ benchmark2.py
@@ -6,6 +6,8 @@ from pointer_selection import enhanced_selection
 from pointer_selection_merge import enhanced_selection_merge
 from merge_sort import mergeSort
 from radix_sort import radixSort
+from bubble_sort import bubbleSort
+from insertion_sort import insertionSort
 
 perfplot.live(
     setup = lambda n: np.random.rand(n).tolist(),
@@ -13,12 +15,21 @@ perfplot.live(
         selectionSort,
         enhanced_selection,
         enhanced_selection_merge,
+        bubbleSort,
+        insertionSort,
         sorted,
         mergeSort,
         radixSort,
     ],
-    labels=["Normal Selection", "Enhanced Selection", "Enhanced Selection Merge", "Default Python Sort", "Normal Merge Sort", "Radix Sort"],
-    n_range=[2**k for k in range(15)],  
+    labels=["Normal Selection", 
+            "Enhanced Selection", 
+            "Enhanced Selection Merge", 
+            "Bubble Sort",
+            "Insertion Sort",
+            "Default Python Sort", 
+            "Normal Merge Sort", 
+            "Radix Sort"],
+    n_range=[2**k for k in range(20)],  
     xlabel="Number of elements",
     equality_check=None
     
