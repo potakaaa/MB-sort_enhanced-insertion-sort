964ea09f2e34de18042b7c46ea4947926edf8483
potakaaa <helbirog@email.com>
2024-11-04 21:40:32 +0800

    enhanced insertion 2 benchmark

--- enhanced_insertion.py
+++ enhanced_insertion.py
@@ -1,34 +1,6 @@
 import random
 from binary_search import binary_search
 
-def enhanced_insertion(arr):
-    p1 = 1
-    p2 = 2
-    for i in range(1, len(arr) - 1):
-        pass
-
-def enhanced_insertion2(arr):
-    n = len(arr) 
-    if n <= 1:
-        return # If the array has 0 or 1 element, it is already sorted
-
-    min_index = 0
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        if arr[i] <= arr[min_index]:
-            arr = [arr[i]] + [arr[min_index]] + arr[min_index:i] + arr[i+1:]
-            min_index = i
-            print(f"I is {i}, arr is {arr}")
-
-        else:
-            pos = binary_search(arr[:i], key)
-            # Shift elements and insert
-            arr = arr[:pos] + [key] + arr[pos:i] + arr[i+1:]
-            print(f"I is {i}, arr is {arr}")
-
-    return arr
-
 def enhanced_insertion3(arr):
     n = len(arr)
     if n <= 1:
@@ -58,26 +30,3 @@ def enhanced_insertion3(arr):
         i += 1
 
     return arr
-
-        
-
-
-
-# list = [2, 2, 3, 1, 4, 2, 3, 2] 
-# newlist = [1, 2, ]
-# binary_search returns 0
-
-# Test the function
-
-arr = list(range(1, 10))
-random.shuffle(arr)
-print("Unsorted array:", arr)
-
-sorted_arr = enhanced_insertion3(arr.copy())
-
-print("Sorted array:", sorted_arr)
-
-print(sorted_arr == sorted(arr))
-
-
-#print(binary_search(arr, 3, 0, 4))
