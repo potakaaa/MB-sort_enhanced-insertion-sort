91d8179e6811443c4d692c0e12736fa99e9b6c52
potakaaa <helbirog@email.com>
2024-11-04 21:25:45 +0800

    enhanced insertion 2 fixed

--- enhanced_insertion.py
+++ enhanced_insertion.py
@@ -28,6 +28,37 @@ def enhanced_insertion2(arr):
             print(f"I is {i}, arr is {arr}")
 
     return arr
+
+def enhanced_insertion3(arr):
+    n = len(arr)
+    if n <= 1:
+        return
+    
+    i = 1
+    min_index = 0
+    
+    while i < n:
+        key = arr[i]
+        j = i - 1
+
+        if key <= arr[min_index]:
+            print("im inside here 1")
+            arr.insert(min_index, arr.pop(i))
+            #arr = [key] + [arr[min_index]] + arr[min_index + 1:i] + arr[i+1:]
+            print(f"I is {i}, arr is {arr}")
+        
+        elif key < arr[j]:
+            print("im inside here 2")
+            pos = binary_search(arr[:i], key)
+            # Shift elements and insert
+            arr.insert(pos, arr.pop(i))
+            #arr = arr[:pos] + [key] + arr[pos:i] + arr[i+1:]
+            print(f"I is {i}, arr is {arr}")
+
+        i += 1
+
+    return arr
+
         
 
 
@@ -38,11 +69,11 @@ def enhanced_insertion2(arr):
 
 # Test the function
 
-arr = list(range(1, 8))
+arr = list(range(1, 10))
 random.shuffle(arr)
 print("Unsorted array:", arr)
 
-sorted_arr = enhanced_insertion2(arr.copy())
+sorted_arr = enhanced_insertion3(arr.copy())
 
 print("Sorted array:", sorted_arr)
 
