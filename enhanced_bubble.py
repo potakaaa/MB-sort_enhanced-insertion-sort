4ee0174165dc79128bf00c950ee6959e9941fd8b
potakaaa <helbirog@email.com>
2024-11-07 16:47:10 +0800

    added benchmark on enhanced_bubble.py

--- enhanced_bubble.py
+++ enhanced_bubble.py
@@ -19,52 +19,58 @@ def enhanced_bubbleSort(arr):
 
     j = 0 #1st pointer - arr[0]
     m = len(arr) - 1 #2nd pointer - arr[-1]
-    print(arr)
     
-
     for i in range(n_2):
         j = i #1st pointer - arr[0]
         m = len(arr) - 1 - i #2nd pointer - arr[-1]
-        print(arr)
 
         for g in range(i):
 
             if arr[j+1] < arr[j]:
-                print(f"1st pointer: {arr[j]}")
                 arr[j+1], arr[j] = arr[j], arr[j+1]
-                print("1st POINTER SWAPPED")
-                print(arr)
             
             if arr[m-1] > arr[m]:
-                print(f"2nd pointer: {arr[m]}")
                 arr[m-1], arr[m] = arr[m], arr[m-1]
-                print("2nd POINTER SWAPPED")
-                print(arr)
             
 
 
+from timeit import timeit
+import random
 
-# Sample list to be sorted
-arr = [39, 12, 72, 10, 2, 18]
-print("-----NORMAL BUBBLE SORT-----")
-print("Unsorted list is:")
-print(arr)
+n = 5000
 
-bubble_sort(arr)
+arr1 = random.choices(range(1, 1000), k = n)
+arr1_copy = arr1.copy()
 
-print("Sorted list is:")
-print(arr)
+t1 = timeit(
+    "bubble_sort(arr1)",
+    setup="from enhanced_bubble import bubble_sort",
+    number = 10,
+    globals={"arr1":arr1},
+)
+
+t2 = timeit(
+    "enhanced_bubbleSort(arr1_copy)",
+    setup="from enhanced_bubble import enhanced_bubbleSort",
+    number = 10,
+    globals={"arr1_copy":arr1_copy},
+)
 
 
-arr2 = [39, 12, 72, 10, 2, 18]
-print("\n-----ENHANCED BUBBLE SORT-----")
-print("Unsorted list is:")
-print(arr2)
 
+l = {t1: "Normal Bubble Sort", t2: "Enhanced Bubble Sort"}
+l_sorted = dict(sorted(l.items()))
+'''print(f"At {n} datas")
+print(f"Fastest: {l.get(min(l))} @ {min(l)}")
+print(f"Slowest: {l.get(max(l))} @ {max(l)}")
+print()'''
 
-enhanced_bubbleSort(arr2)
-
-print("Sorted list is:")
-print(arr2)
+print()
+i = 1
+print(f"At {n} datas")
+for key, value in l_sorted.items():
+    print(f"{i}. {value} @ {key} seconds")
+    i += 1
+print()
 
 
