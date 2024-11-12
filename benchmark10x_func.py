b74ec9fb6499a6e32044fcb5b1d098b94f91ec02
potakaaa <helbirog@email.com>
2024-11-12 21:17:57 +0800

    modularized test10x functions

--- benchmark10x_func.py
+++ benchmark10x_func.py
@@ -4,8 +4,8 @@ from normal_algo.insertion_sort import insertionSort
 from enhanced_insertion import enhanced_insertion
 from nearly_sort import nearly_sorted
 
-def test_10x(func, name, r, n):
-    print(f"Benchmark for {name} \n@ {n} datas ranging from 1 - {r}")
+def test_10x(func, name, rand, r, n):
+    print(f"Benchmark for {name} \n@ {n} datas ranging from 1 - {r} -- {rand} datas")
     for i in range(1,11):
         arr1 = random.choices(range(1, r), k = n)
         t1 = timeit(
@@ -16,8 +16,8 @@ def test_10x(func, name, r, n):
         print(f"{t1}")
     print()
 
-def test_10x_n_sorted(func, name, r, n):
-    print(f"Benchmark for {name} \n@ {n} sorted datas ranging from 1 - {r}")
+def test_10x_n_sorted(func, name, rand, r, n):
+    print(f"Benchmark for {name} \n@ {n} sorted datas ranging from 1 - {r} -- {rand} datas")
     for i in range(1,11):
         arr1 = random.choices(range(1, r), k = n)
         arr1 = nearly_sorted(arr1)
@@ -29,11 +29,25 @@ def test_10x_n_sorted(func, name, r, n):
         print(f"{t1}")
     print()
 
-def test_10x_sorted(func, name, r, n):
-    print(f"Benchmark for {name} \n@ {n} sorted datas ranging from 1 - {r}")
+def test_10x_sorted(func, name, rand, r, n):
+    print(f"Benchmark for {name} \n@ {n} sorted datas ranging from 1 - {r} -- {rand} datas")
     for i in range(1,11):
         arr1 = random.choices(range(1, r), k = n)
         arr1.sort()
+        t1 = timeit(
+        lambda: func(arr1),
+        number = 1,
+        globals={"arr1": arr1, "func": func},
+        )
+        print(f"{t1}")
+    print()
+
+def test_10x_r_sorted(func, name, rand, r, n):
+    print(f"Benchmark for {name} \n@ {n} sorted datas ranging from 1 - {r} -- {rand} datas")
+    for i in range(1,11):
+        arr1 = random.choices(range(1, r), k = n)
+        arr1.sort()
+        arr1.reverse()
         t1 = timeit(
         lambda: func(arr1),
         number = 1,
--- benchmark_run10x.py
+++ benchmark_run10x.py
@@ -2,14 +2,59 @@ from benchmark10x_func import *
 
 test_range = 10
 
+rand = ["Random", "Sorted", "Nearly Sorted", "Reverse Sorted"]
+
+print("For random datas:")
 # 1-10 at 10 datas
-test_10x(enhanced_insertion, "MB Sort", test_range, 10)
-test_10x(insertionSort, "Insertion Sort", test_range, 10)
+test_10x(enhanced_insertion, "MB Sort", rand[0], test_range, 10)
+test_10x(insertionSort, "Insertion Sort", rand[0], test_range, 10)
 
 # 1-10 at 1_000 datas 
-test_10x(enhanced_insertion, "MB Sort", test_range, 1_000)
-test_10x(insertionSort, "Insertion Sort", test_range, 1_000)
+test_10x(enhanced_insertion, "MB Sort", rand[0], test_range, 1_000)
+test_10x(insertionSort, "Insertion Sort", rand[0], test_range, 1_000)
 
 # 1-10 at 100_000 datas
-test_10x(enhanced_insertion, "MB Sort", test_range, 100_000)
-test_10x(insertionSort, "Insertion Sort", test_range, 100_000)
+test_10x(enhanced_insertion, "MB Sort", rand[0], test_range, 100_000)
+test_10x(insertionSort, "Insertion Sort", rand[0], test_range, 100_000)
+
+
+print("For sorted datas:")
+# 1-10 at 10 datas
+test_10x_sorted(enhanced_insertion, "MB Sort", rand[1], test_range, 10)
+test_10x_sorted(insertionSort, "Insertion Sort", rand[1], test_range, 10)
+
+# 1-10 at 1_000 datas 
+test_10x_sorted(enhanced_insertion, "MB Sort", rand[1], test_range, 1_000)
+test_10x_sorted(insertionSort, "Insertion Sort", rand[1], test_range, 1_000)
+
+# 1-10 at 100_000 datas
+test_10x_sorted(enhanced_insertion, "MB Sort", rand[1], test_range, 100_000)
+test_10x_sorted(insertionSort, "Insertion Sort", rand[1], test_range, 100_000)
+
+
+print("For nearly sorted datas:")
+# 1-10 at 10 datas
+test_10x_n_sorted(enhanced_insertion, "MB Sort", rand[2], test_range, 10)
+test_10x_n_sorted(insertionSort, "Insertion Sort", rand[2], test_range, 10)
+
+# 1-10 at 1_000 datas 
+test_10x_n_sorted(enhanced_insertion, "MB Sort", rand[2], test_range, 1_000)
+test_10x_n_sorted(insertionSort, "Insertion Sort", rand[2], test_range, 1_000)
+
+# 1-10 at 100_000 datas
+test_10x_n_sorted(enhanced_insertion, "MB Sort", rand[2], test_range, 100_000)
+test_10x_n_sorted(insertionSort, "Insertion Sort", rand[2], test_range, 100_000)
+
+
+print("For reversed sorted datas:")
+# 1-10 at 10 datas
+test_10x_r_sorted(enhanced_insertion, "MB Sort", rand[3], test_range, 10)
+test_10x_r_sorted(insertionSort, "Insertion Sort", rand[3], test_range, 10)
+
+# 1-10 at 1_000 datas 
+test_10x_r_sorted(enhanced_insertion, "MB Sort", rand[3], test_range, 1_000)
+test_10x_r_sorted(insertionSort, "Insertion Sort", rand[3], test_range, 1_000)
+
+# 1-10 at 100_000 datas
+test_10x_r_sorted(enhanced_insertion, "MB Sort", rand[3], test_range, 100_000)
+test_10x_r_sorted(insertionSort, "Insertion Sort", rand[3], test_range, 100_000)
