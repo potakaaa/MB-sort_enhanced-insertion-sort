9773a8be97a00f0487ae3dc1ec1ff75b42ae0d14
potakaaa <helbirog@email.com>
2024-11-12 10:43:29 +0800

    added 10x benchmark

--- benchmark_run10x.py
+++ benchmark_run10x.py
@@ -15,7 +15,7 @@ t1 = timeit(
     globals={"arr1":arr1},
 )
 
-def test_30x(func, name, r, n):
+def test_10x(func, name, r, n):
     print(f"Benchmark for {name} \n@ {n} datas ranging from 1 - {r}")
     for i in range(1,11):
         arr1 = random.choices(range(1, r), k = n)
@@ -27,8 +27,18 @@ def test_30x(func, name, r, n):
         print(f"{t1}")
     print()
 
-normal_ins_imp = "from normal_algo.insertion_sort import insertionSort"
-enhanced_ins_imp = "from enhanced_insertion import enhanced_insertion"
+# 1-10 at 10 datas
+test_10x(enhanced_insertion, "MB Sort", 10, 10)
+test_10x(insertionSort, "Insertion Sort", 10, 10)
 
-test_30x(enhanced_insertion, "MB Sort", 10, 10)
-test_30x(insertionSort, "Insertion Sort", 10, 10)
+# 1-10 at 1_000 datas
+test_10x(enhanced_insertion, "MB Sort", 10, 10)
+test_10x(insertionSort, "Insertion Sort", 10, 10)
+
+# 1-10 at 100_000 datas
+test_10x(enhanced_insertion, "MB Sort", 10, 10)
+test_10x(insertionSort, "Insertion Sort", 10, 10)
+
+# 1-10 at 1_000_000 datas
+test_10x(enhanced_insertion, "MB Sort", 10, 10)
+test_10x(insertionSort, "Insertion Sort", 10, 10)
